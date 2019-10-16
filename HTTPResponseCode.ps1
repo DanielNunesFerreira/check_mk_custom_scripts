a14489193148495814acf57dbfd9cf4af98c9a9d
DanielNunesFerreira <40760760+DanielNunesFerreira@users.noreply.github.com>
2019-10-16 12:07:39 +0100

    Update HTTPResponseCode.ps1

--- HTTPResponseCode.ps1
+++ HTTPResponseCode.ps1
@@ -4,7 +4,7 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 try{
 $request = $null
-$request = Invoke-WebRequest -Uri "https://someurl" -UseBasicParsing -UseDefaultCredential
+$request = Invoke-WebRequest -Uri "https://SomeURL.com" -UseBasicParsing -UseDefaultCredential
 } 
 catch
 {              
@@ -20,4 +20,4 @@ if ($StatusCode -like 200) {
 $status = 0 #ok
 }
 
-"$status HTTPResponseCode-Confluence http_responsecode=$StatusCode;;;; HTTP response code $StatusCode"
+"$status HTTPResponseCode-SomeURL http_responsecode=$StatusCode;;;; HTTP response code $StatusCode"
