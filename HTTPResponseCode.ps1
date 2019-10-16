62c169209a6eed04f79a000d253feb0bdf1382c3
DanielNunesFerreira <40760760+DanielNunesFerreira@users.noreply.github.com>
2019-10-16 12:05:41 +0100

    Create HTTPResponseCode.ps1
    Simple HTTP Response Code check in Powershell

---
+++ HTTPResponseCode.ps1
@@ -0,0 +1,23 @@
+# Add the script to the local scripts folder in the HOST Machine
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+try{
+$request = $null
+$request = Invoke-WebRequest -Uri "https://someurl" -UseBasicParsing -UseDefaultCredential
+} 
+catch
+{              
+$request = $_.Exception.Response            
+}  
+$StatusCode = [int] $request.StatusCode;
+$StatusDescription = $request.StatusDescription;
+
+
+$status = 2 #critical
+
+if ($StatusCode -like 200) {
+$status = 0 #ok
+}
+
+"$status HTTPResponseCode-Confluence http_responsecode=$StatusCode;;;; HTTP response code $StatusCode"
